e4df25f7c85b8f487361f1ed1b09fb4e8f6bdcf8
Renan Cardoso Segretti <renanony@gmail.com>
2024-02-16 18:26:49 -0300

    Refazendo de cabeça

--- js/app.js
+++ js/app.js
@@ -1,23 +1,15 @@
-function alterarStatus(btn,img) {
-    let botao = document.getElementById(btn);
-    let imagem = document.getElementById(img);
-    // Pegando o Botão e a Imagem do HTML.
+function alterarStatus(id) {
+    let lista = document.getElementById(`game-${id}`);
+    let botao = lista.querySelector('.dashboard__item__button');
+    let imagem = lista.querySelector('.dashboard__item__img');
 
     if (botao.classList.contains('dashboard__item__button--return')) {
-        // Se o botão for da classe desabilitada...
         botao.classList.remove('dashboard__item__button--return');
         imagem.classList.remove('dashboard__item__img--rented');
-        //...remove a classe desabilitada.
-        botao.innerHTML = 'Alugar';
-        // E muda o texto do botão.
-        
+        botao.textContent = 'Alugar';
     } else {
-        botao.classList.add('dashboard__item__button--return');
-        imagem.classList.add('dashboard__item__img--rented');
-        // Se não, adiciona a classe desabilitada.
-        botao.innerHTML = 'Devolver';
-        // E muda o texto do botão.
-    }
+            botao.classList.add('dashboard__item__button--return');
+            imagem.classList.add('dashboard__item__img--rented');
+            botao.textContent = 'Devolver';
+        }
 }
-
-.
